27e554e432ee857f66741f141fd3951b22b8a134
Naresh Chaurasia <johndoe@example.com>
2024-07-31 09:30:01 +0530

    Macbook Pro, Checkin Timestamp::Wed Jul 31 09:30:01 IST 2024

---
+++ core-java/src/AssignmentOperatorsDemo.java
@@ -0,0 +1,11 @@
+public class AssignmentOperatorsDemo {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 0;
+
+
+        // Addition assignment
+        b += a; //c = c + b
+        System.out.println("Addition assignment: c += b -> c = " + b);
+    }
+}
---
+++ core-java/src/TernaryOperatorDemo.java
@@ -0,0 +1,9 @@
+public class TernaryOperatorDemo {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        boolean isEqual = (a == b) ? true : false;
+        System.out.println("a equals b: " + isEqual);
+    }
+}
