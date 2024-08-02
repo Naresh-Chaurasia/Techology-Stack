042e7c18243156ff1e07cb4d58aad1b17c21efb0
Naresh Chaurasia <johndoe@example.com>
2024-08-02 14:40:15 +0530

    Macbook Pro, Checkin Timestamp::Fri Aug  2 14:40:15 IST 2024

---
+++ core-java/src/ForLoop.java
@@ -0,0 +1,19 @@
+public class ForLoop {
+    public static void main(String[] args) {
+        
+        int a = 5;
+
+    //     while(a<5){
+    //         System.out.println(a++);
+    //     }
+    //    System.out.println("outside while loop:"+a);
+
+        do{
+            System.out.println(a);
+            ++a;
+        }while(a<5);
+
+        
+
+    }
+}
