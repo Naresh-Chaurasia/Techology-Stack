f31baa354fe0e082c2884605998c59a421034ac4
Naresh Chaurasia <johndoe@example.com>
2024-08-03 15:50:27 +0530

    Macbook Pro, Checkin Timestamp::Sat  3 Aug 2024 15:50:27 IST

---
+++ core-java/src/Main1.java
@@ -0,0 +1,9 @@
+
+
+public class Main1 {
+    public static void main(String[] args) {
+
+        int a = 10;//right to left
+       
+    }
+}
---
+++ core-java/src/OOP.java
@@ -0,0 +1,22 @@
+
+class Calculator{
+    
+    int price;
+
+    void add(int a, int b){
+        System.out.println(a+b);
+    }
+
+}
+
+
+public class OOP{
+    public static void main(String[] args) {
+       
+        int a = 10;
+        Calculator c = new Calculator();
+        c.add(10, 20);
+
+    }
+
+}//end of class
