72015970d0cb5f3e6dd2b910839d36e8420b5ebc
Naresh Chaurasia <johndoe@example.com>
2024-08-10 16:27:19 +0530

    Macbook Pro, Checkin Timestamp::Sat 10 Aug 2024 16:27:19 IST

---
+++ automation-architect/core-java/src/india1/StringInJava.java
@@ -0,0 +1,22 @@
+package india1;
+
+public class StringInJava {
+    public static void main(String[] args) {
+        
+        //Object Creation for String
+        //String name = new String( "Java Programming");
+        String name = "Java";
+
+        char c = name.charAt(0);
+        System.out.println(c);
+
+        int index1 = name.indexOf('a');
+        System.out.println(index1);
+
+        int index2 = name.lastIndexOf('a');
+        System.out.println(index2);
+
+        
+
+    }
+}
