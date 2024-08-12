c9d82333d2420e8c23c0536f2680a1801c7c927e
Naresh Chaurasia <johndoe@example.com>
2024-08-12 22:14:16 +0530

    Macbook Pro, Checkin Timestamp::Mon 12 Aug 2024 22:14:16 IST

---
+++ automation-architect/core-java/src/uk1/WhileLoop.java
@@ -0,0 +1,17 @@
+package uk1;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+        
+        int a = 5;
+
+        while(a<5)
+        {
+            System.out.println(a);
+            ++a;
+        }
+
+        System.out.println("End of loop...");
+
+    }    
+}
