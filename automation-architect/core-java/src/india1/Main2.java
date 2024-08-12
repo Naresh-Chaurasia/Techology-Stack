aaf67e76be2c03c4222a45c75729389c69192cc4
Naresh Chaurasia <johndoe@example.com>
2024-08-12 21:48:11 +0530

    Macbook Pro, Checkin Timestamp::Mon 12 Aug 2024 21:48:11 IST

---
+++ automation-architect/core-java/src/india1/Main2.java
@@ -0,0 +1,27 @@
+package india1;
+
+public class Main2 {
+    public static void main(String[] args) {
+
+        String name1="Ch ";
+        String name2="Ch";
+
+        boolean bool = name1.equals(name2);
+        System.out.println(bool);
+
+        
+
+        String str = "Hello,World!";
+
+        
+
+        // Check equality
+        boolean isEqual = str.equals("Hello, World!");
+        System.out.println("Is Equal: " + isEqual);  // Output: true
+
+        
+
+    }
+}
+
+
