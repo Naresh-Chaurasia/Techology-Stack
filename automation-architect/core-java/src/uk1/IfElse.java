fdc58b47cbd0873ef2ae9622ea9b146fcd447886
Naresh Chaurasia <johndoe@example.com>
2024-08-12 22:01:54 +0530

    Macbook Pro, Checkin Timestamp::Mon 12 Aug 2024 22:01:54 IST

--- automation-architect/core-java/src/uk1/IfElse.java
+++ automation-architect/core-java/src/uk1/IfElse.java
@@ -1,14 +1,21 @@
+package uk1;
+
 public class IfElse {
 
     public static void main(String[] args) {
         int a = 10;
 
-        
-        if(a > 0){
+        if (a > 0) 
+        {
             System.out.println("a is +ve");
-        }else {
+        } 
+        else if (a==0)
+        {
+            System.out.println("a is neither +ve nor -ve");
+        } 
+        else 
+        {
             System.out.println("a is -ve");
         }
     }
-    
 }
