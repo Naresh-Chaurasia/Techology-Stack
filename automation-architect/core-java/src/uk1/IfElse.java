fc66e87041cf5c99695e19d8a967103480c1a131
Naresh Chaurasia <johndoe@example.com>
2024-08-12 21:54:03 +0530

    Macbook Pro, Checkin Timestamp::Mon 12 Aug 2024 21:54:03 IST

---
+++ automation-architect/core-java/src/uk1/IfElse.java
@@ -0,0 +1,14 @@
+public class IfElse {
+
+    public static void main(String[] args) {
+        int a = 10;
+
+        
+        if(a > 0){
+            System.out.println("a is +ve");
+        }else {
+            System.out.println("a is -ve");
+        }
+    }
+    
+}
