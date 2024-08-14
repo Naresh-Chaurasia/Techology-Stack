7ca86d92805df5da1e64812caf0a297a1b6ba6c8
Naresh Chaurasia <johndoe@example.com>
2024-08-14 22:02:28 +0530

    Macbook Pro, Checkin Timestamp::Wed 14 Aug 2024 22:02:28 IST

---
+++ automation-architect/core-java/src/uk1/WhatIsMethod.java
@@ -0,0 +1,24 @@
+
+package uk1;
+
+public class WhatIsMethod {
+    public static void main(String[] args) {
+        System.out.println("Hello...");
+
+        makeCoffee();
+        makeCoffee();
+        makeCoffee();
+        makeCoffee();
+
+    }
+
+
+    static void makeCoffee(){
+        System.out.println("--------");
+         System.out.println("Warm water");
+         System.out.println("add coffee");
+         System.out.println("add sugar");
+         System.out.println("add milk");
+    }
+
+}
