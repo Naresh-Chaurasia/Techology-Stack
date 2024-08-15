9e490b49975c4668bc8a6811d6e8e6a5daea71a6
Naresh Chaurasia <johndoe@example.com>
2024-08-15 17:39:49 +0530

    Macbook Pro, Checkin Timestamp::Thu 15 Aug 2024 17:39:49 IST

--- automation-architect/core-java/src/india1/EmployeeCall.java
+++ automation-architect/core-java/src/india1/EmployeeCall.java
@@ -1,18 +1,23 @@
 package india1;
 
 public class EmployeeCall {
+
+    String name = "NC";
+    static String company="CTS";
+
     public static void main(String[] args) {
-        EmployeeCall obj = new EmployeeCall();
-        obj.method1();
-
-        method1();
-
-        EmployeeCall.method1();
+       method1();
     }   
     
+    void method2(){
+        System.out.println(name);
+        System.out.println(company);
+    }
     
     static void method1(){
         System.out.println("I am static method...");
+        System.out.println(company);
+        //System.out.println(name);
     }
 
 }
