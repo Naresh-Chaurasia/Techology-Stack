b2466e2bdf5d1308c2eeeae8d6b76c95b69c872c
Naresh Chaurasia <johndoe@example.com>
2024-08-15 17:51:50 +0530

    Macbook Pro, Checkin Timestamp::Thu 15 Aug 2024 17:51:50 IST

---
+++ automation-architect/core-java/src/india1/InheritConcept.java
@@ -0,0 +1,8 @@
+package india1;
+
+public class InheritConcept {
+    public static void main(String[] args) {
+        Bus b = new Bus();
+        b.start();
+    }
+}
---
+++ automation-architect/core-java/src/india1/Vehicle.java
@@ -0,0 +1,7 @@
+package india1;
+
+public class Vehicle {
+    void start() {
+        System.out.println("Vehicle is starting");
+    }
+}
