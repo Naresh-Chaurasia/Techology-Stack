8231387634f27e68497966055d75568429ffbb34
Naresh Chaurasia <johndoe@example.com>
2024-08-17 16:34:37 +0530

    Macbook Pro, Checkin Timestamp::Sat Aug 17 16:34:37 IST 2024

--- automation-architect/core-java/src/india1/ExceptionHandling.java
+++ automation-architect/core-java/src/india1/ExceptionHandling.java
@@ -3,24 +3,22 @@ package india1;
 public class ExceptionHandling {
     public static void main(String[] args) {
         int a = 10;
-        int b = 0;
+        int b = 2;
         int c = 0;
-        System.out.println( "Hello World!" );
+        System.out.println("Hello World!");
 
-
-        try{
+        try {
             System.out.println("Before division...");
-            c = a/b;
+            c = a / b;
             System.out.println("After division...");
-        }
-        catch(Exception e){
+        } catch (Exception e) {
             System.out.println(e.toString());
+        } finally {
+            System.out.println("This block always executes.");
         }
 
-        
+        System.out.println(c);
 
-        System.out.println( c );
-
-        System.out.println( "I am fine. How are you !!!" );
+        System.out.println("I am fine. How are you !!!");
     }
 }
