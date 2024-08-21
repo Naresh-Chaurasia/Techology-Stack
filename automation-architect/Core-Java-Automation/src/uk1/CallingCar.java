596463cf934ea0fcdf48fcf8014524daf1b571ed
Naresh Chaurasia <johndoe@example.com>
2024-08-21 17:17:17 +0530

    Macbook Pro, Checkin Timestamp::Wed 21 Aug 2024 17:17:17 IST

--- automation-architect/Core-Java-Automation/src/uk1/CallingCar.java
+++ automation-architect/Core-Java-Automation/src/uk1/CallingCar.java
@@ -2,16 +2,26 @@ package uk1;
 
 public class CallingCar {
     public static void main(String[] args) {
-        Car c = new Car();
+
+        Car c = new Car("Hyundai");
+        
         c.color = "red";
         c.model = 2024;
 
+        Car c2 = new Car("Hyundai");
+        
+        c2.color = "blue";
+        c2.model = 2023;
+
+
+        
         // System.out.println(c.color);
         // System.out.println(c.model);
 
         // c.start();
         // c.stop();
         c.display();
+        c2.display();
 
 
     }
--- automation-architect/Core-Java-Automation/src/uk1/Car.java
+++ automation-architect/Core-Java-Automation/src/uk1/Car.java
@@ -4,6 +4,12 @@ public class Car {
 
     String color;
     int model;
+    String company;
+
+    //No return type.
+    Car(String c){
+        company = c;
+    }
 
     void start(){
         System.out.println("Starting Car...");
@@ -16,6 +22,7 @@ public class Car {
     void display(){
         System.out.println(color);
         System.out.println(model);
+        System.out.println(company);
     }
     
 }
