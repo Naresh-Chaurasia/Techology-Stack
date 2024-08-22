d11f2203e7bf126c08576d62394400bdc53a0f61
Naresh Chaurasia <johndoe@example.com>
2024-08-22 22:39:17 +0530

    Macbook Pro, Checkin Timestamp::Thu 22 Aug 2024 22:39:17 IST

--- automation-architect/Core-Java-Automation/src/uk1/BMWCar.java
+++ automation-architect/Core-Java-Automation/src/uk1/BMWCar.java
@@ -1,6 +1,11 @@
 package uk1;
 
 public class BMWCar extends Vehicle{
+
+    void start() {
+        System.out.println("Vehicle is starting with power...");
+    } 
+
     void honk() {
         System.out.println("Car is honking");
     }
--- automation-architect/Core-Java-Automation/src/uk1/UsingVehicle.java
+++ automation-architect/Core-Java-Automation/src/uk1/UsingVehicle.java
@@ -4,12 +4,19 @@ public class UsingVehicle {
 
     public static void main(String[] args) {
     
-        Vehicle v = new Vehicle();
-        v.start();
+        // Vehicle v = new Vehicle();
+        // v.start();
     
-        BMWCar b = new BMWCar();
-        b.honk();
-        b.start();
+        // BMWCar b = new BMWCar();
+        // b.honk();
+        // b.start();
+
+
+        
+
+        Vehicle b2 = new BMWCar();
+
+        b2.start();
     
     }
     
