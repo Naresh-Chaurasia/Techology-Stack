8328f1544fe9f4548e25537ac9e193c926783093
Naresh Chaurasia <johndoe@example.com>
2024-08-23 16:17:31 +0530

    Macbook Pro, Checkin Timestamp::Fri Aug 23 16:17:31 IST 2024

---
+++ automation-architect/Core-Java-Automation/src/looping/SampleCode.java
@@ -0,0 +1,19 @@
+package looping;
+
+/*
+ * 
+ * 
+ */
+public class SampleCode {
+    public static void main(String[] args) {
+        int a = 10;
+
+        int b = 20;
+
+        if (a > 5) {
+            System.out.println("A");
+        } else {
+
+        }
+    }
+}
