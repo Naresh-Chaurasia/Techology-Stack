d4ffa63a1470b110b4096e7b9412746356d5296f
Naresh Chaurasia <johndoe@example.com>
2024-08-28 22:39:23 +0530

    Macbook Pro, Checkin Timestamp::Wed Aug 28 22:39:23 IST 2024

--- automation-architect/Core-Java-Automation/src/oops1/Main.java
+++ automation-architect/Core-Java-Automation/src/oops1/Main.java
@@ -2,14 +2,21 @@ package oops1;
 
 public class Main {
     public static void main(String[] args) {
+        // Employee e1 = new Employee("Punit");
+        // e1.companyName = "CTS";
+        
+        // Employee e2 = new Employee("NC");
+        // System.out.println(e2.companyName);
+
+        // e2.companyName = "TCS";
+        // System.out.println(e1.companyName);
+
         Employee e1 = new Employee("Punit");
         e1.companyName = "CTS";
-        
-        Employee e2 = new Employee("NC");
-        System.out.println(e2.companyName);
 
-        e2.companyName = "TCS";
-        System.out.println(e1.companyName);
+        Employee e2 = new Employee("NC");
+
+        e2.showEmployeeDetails();
     
     }
 }
