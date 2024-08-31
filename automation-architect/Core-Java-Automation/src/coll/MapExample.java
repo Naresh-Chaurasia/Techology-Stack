d9d1fd9bd1e830188aaa5862722fad5227df9624
Naresh Chaurasia <johndoe@example.com>
2024-08-31 12:19:02 +0530

    Macbook Pro, Checkin Timestamp::Sat Aug 31 12:19:02 IST 2024

---
+++ automation-architect/Core-Java-Automation/src/coll/MapExample.java
@@ -0,0 +1,23 @@
+package coll;
+
+import java.util.HashMap;
+
+public class MapExample {
+    public static void main(String[] args) {
+        HashMap <String, String> hm = new HashMap<String, String>();
+
+
+        hm.put("100", "NC");
+
+        hm.put("200", "Punit");
+
+        System.out.println(hm);
+
+        String val = hm.get("100");
+        System.out.println(val);
+
+        String no_val = hm.get("300");
+        System.out.println(no_val);
+
+    }
+}
