47b0e28ea60872537c63c7f11923f87637ff670d
Naresh Chaurasia <johndoe@example.com>
2024-08-31 11:48:42 +0530

    Macbook Pro, Checkin Timestamp::Sat Aug 31 11:48:42 IST 2024

---
+++ automation-architect/Core-Java-Automation/src/coll/AList.java
@@ -0,0 +1,20 @@
+package coll;
+
+import java.util.ArrayList;
+
+public class AList {
+
+    public static void main(String[] args) {
+        
+        ArrayList <String> fruits = new ArrayList<String>();
+
+        // Adding elements to the ArrayList
+        fruits.add("Apple");
+        fruits.add("Banana");
+        fruits.add("Mango");
+
+        System.out.println(fruits);
+
+    }
+    
+}
